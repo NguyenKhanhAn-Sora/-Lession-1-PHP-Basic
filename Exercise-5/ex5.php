30ff75144c9f4cbb7e9470cda6eead65cfc4a18d
Nguyen Khanh An <soranguyen1211@gmail.com>
2025-01-15 20:40:25 +0700

    first upload

---
+++ Exercise-5/ex5.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <style>
+        p {
+            margin: 0;
+        }
+    </style>
+    <?php
+    
+    echo "<h2 style='text-align:center; font-size: 30px; line-height: 1.6; padding-bottom:15px; margin-bottom: 15px'>This heading is in the center</h2>";
+    echo "<p style='font-size: 16px; line-height:1.6;  border-bottom: 1px solid #ccc; padding-bottom: 15px; margin-bottom: 15px'>This is paragraph one had should be on top</p>";
+    echo "<p>Hello</p>";
+    echo "<p>You delivered your assignment ontime.</p>";
+    echo "<p>Thanks</p>";
+    echo "<p style='padding-bottom:15px; margin-bottom: 15px;  border-bottom: 1px solid #ccc'>Mahnaz</p>";
+    echo "<p>This paragraph two and should be at bottom</p>";
+    ?>
+</body>
+</html>
