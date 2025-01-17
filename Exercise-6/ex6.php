5549c704a6093961e64216d3083b3e38f4db030e
Nguyen Khanh An <soranguyen1211@gmail.com>
2025-01-17 18:14:04 +0700

    upload

---
+++ Exercise-6/ex6.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        * {
+            font-family: Arial, Helvetica, sans-serif;
+        }
+    </style>
+</head>
+<body>
+
+
+<?php
+    
+    echo "<div style='width: 320px;height=auto'>
+    
+    <img src='./z6232078068533_e0b9f1b4639a88505302967e38071654.jpg' style='width:100%'>
+    <h2>Máy ảnh Sony RX100 IV (Chính Hãng) </h2>
+    <span style= 'font-size:24px;font-weight:700; padding-right:10px'>19.090.000 ₫</span>
+
+    <span style='text-decoration:line-throught; font-size:16px;color:#aaa; padding-right: 10px'>22.990.000 ₫</span>
+
+    <span style='color:red; font-size:16px; font-weight: 800'> -17%</span>
+
+    </div>"
+    
+    ?>
+</body>
+</html>
